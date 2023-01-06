2e44861bd026a751226b57ab1f92787456dcc801
Alex Paden <padenfool@gmail.com>
2023-01-06 09:21:10 -0600

    Remove bug.ts router, add content to project.ts
    Easier management for routing

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,19 +2,12 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 import projectRoutes from './routes/project';
-import bugRoutes from './routes/bug';
 
-// create express app and register routes
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_request, response) => {
-    return response.json({ message: 'Hello World' });
-});
-
 app.use('/projects', projectRoutes);
-app.use('/projects', bugRoutes);
 
 export default app;
--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -1,10 +1,6 @@
 import express from 'express';
-import {
-    getProjects,
-    createProject,
-    editProject,
-    deleteProject
-} from '../controllers/project';
+import { getProjects, createProject, editProject, deleteProject } from '../controllers/project';
+import { getBugs, createBug, updateBug, deleteBug } from '../controllers/bug';
 
 const router = express.Router();
 
@@ -12,5 +8,10 @@ router.get('/', getProjects);
 router.post('/', createProject);
 router.put('/:id', editProject);
 router.delete('/:id', deleteProject);
+
+router.get('/:projectId/bugs', getBugs);
+router.post('/:projectId/bugs', createBug);
+router.put('/:projectId/bugs/:bugId', updateBug);
+router.delete('/:projectId/bugs/:bugId', deleteBug);
 
 export default router;
