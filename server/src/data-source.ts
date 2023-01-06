871ffcb5ffa2e06895da38cf0f51b5633c1b190d
Alex Paden <padenfool@gmail.com>
2023-01-06 09:55:56 -0600

    Add notes routing and include Notes in data-source.ts
    working notes routes :+1:

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -2,13 +2,14 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Project } from "./entity/Project"
 import { Bug } from "./entity/Bug"
+import { Note } from "./entity/Note"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: [Project, Bug],
+    entities: [Project, Bug, Note],
     migrations: [],
     subscribers: [],
 })
--- server/src/routes/project.ts
+++ server/src/routes/project.ts
@@ -1,17 +1,27 @@
 import express from 'express';
 import { getProjects, createProject, editProject, deleteProject } from '../controllers/project';
 import { getBugs, createBug, updateBug, deleteBug } from '../controllers/bug';
+import { getNotes, postNote, deleteNote, updateNote } from '../controllers/note';
+
 
 const router = express.Router();
 
+// Project routes
 router.get('/', getProjects);
 router.post('/', createProject);
 router.put('/:id', editProject);
 router.delete('/:id', deleteProject);
 
+// Project bug routes
 router.get('/:projectId/bugs', getBugs);
 router.post('/:projectId/bugs', createBug);
 router.put('/:projectId/bugs/:bugId', updateBug);
 router.delete('/:projectId/bugs/:bugId', deleteBug);
+
+// Project note routes
+router.get('/:projectId/bugs/:bugId/notes', getNotes);
+router.post('/:projectId/bugs/:bugId/notes', postNote);
+router.delete('/:projectId/notes/:noteId', deleteNote);
+router.put('/:projectId/notes/:noteId', updateNote);
 
 export default router;
