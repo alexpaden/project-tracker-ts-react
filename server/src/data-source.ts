2fffc13f3eedf05ecdf6252d403da047eca0c0a4
Alex Paden <padenfool@gmail.com>
2023-01-05 19:58:10 -0600

    Update data-source.ts with Bug
    Bug Entity

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { Project } from "./entity/Project"
+import { Bug } from "./entity/Bug"
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: [Project],
+    entities: [Project, Bug],
     migrations: [],
     subscribers: [],
 })
