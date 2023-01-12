7af385132702abffc1702ec2caa506fb9f4b1d53
Alex Paden <padenfool@gmail.com>
2023-01-12 15:08:31 -0600

    Create projects page

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,31 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import React from 'react'
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
+import ProjectsPage from './pages/ProjectsPage'
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 const rootElement = document.getElementById('root')
 if (!rootElement) throw new Error('Failed to find the root element')
 const root = createRoot(rootElement)
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/projects',
+    element: <ProjectsPage />,
+  },
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 )
 
---
+++ client/src/pages/ProjectsPage.tsx
@@ -0,0 +1,24 @@
+import { useReducer } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProjects, selectProjectsState } from '../redux/slices/projectsSlice';
+
+
+const ProjectsPage = () => {
+    const dispatch = useDispatch();
+    dispatch(fetchProjects());
+    const { projects } = useSelector(selectProjectsState);
+
+    return (
+        <div>
+            <h1>Projects</h1>
+            <ul>
+                {projects.map((project) => (
+                    <li key={project.id}>{project.name}</li>
+                ))}
+            </ul>
+            
+        </div>
+    );
+};
+
+export default ProjectsPage;
