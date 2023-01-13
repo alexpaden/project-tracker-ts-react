5294b389993dcb5ef88d323bc27e25ea5a9886f5
Alex Paden <padenfool@gmail.com>
2023-01-13 17:59:44 -0600

    Create Member.ts Entity

---
+++ server/src/entity/Member.ts
@@ -0,0 +1,32 @@
+import {
+  BaseEntity,
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm'
+import { Project } from './Project'
+import { User } from './User'
+
+@Entity({ name: 'members' })
+export class Member extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @ManyToOne(() => Project, (project) => project)
+  @JoinColumn({ name: 'projectId' })
+  project: Project
+  @Column()
+  projectId: string
+
+  @ManyToOne(() => User, (user) => user)
+  @JoinColumn({ name: 'memberId' })
+  member: User
+  @Column()
+  memberId: string
+
+  @CreateDateColumn()
+  joinedAt: Date
+}
