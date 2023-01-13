61100ad55dbb4972e2f2b1ce40dd5522a70326ed
Alex Paden <padenfool@gmail.com>
2023-01-13 08:58:22 -0600

    create projectSlice error handler

--- client/src/redux/slices/projectsSlice.ts
+++ client/src/redux/slices/projectsSlice.ts
@@ -2,13 +2,20 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { RootState, AppThunk } from '../store'
 import projectService from '../../services/projectsService'
 import { ProjectState } from '../types'
+import { getErrorMsg } from '../../utils/helperFuncs'
 
 interface InitialProjectsState {
   projects: ProjectState[]
+  fetchError: string | null
+  fetchStatus: 'idle' | 'loading' | 'succeeded' | 'failed'
+  submitError: string | null
 }
 
 const initialState: InitialProjectsState = {
   projects: [],
+  fetchStatus: 'idle',
+  fetchError: null,
+  submitError: null,
 }
 
 const projectsSlice = createSlice({
@@ -17,19 +24,25 @@ const projectsSlice = createSlice({
   reducers: {
     setProjects: (state, action: PayloadAction<ProjectState[]>) => {
       state.projects = action.payload
+      state.fetchStatus = 'succeeded'
+      state.fetchError = null
+    },
+    setFetchProjectsError: (state, action: PayloadAction<string>) => {
+      state.fetchStatus = 'failed'
+      state.fetchError = action.payload
     },
   },
 })
 
-export const { setProjects } = projectsSlice.actions
+export const { setProjects, setFetchProjectsError } = projectsSlice.actions
 
 export const fetchProjects = (): AppThunk => {
   return async (dispatch) => {
     try {
       const allProjects = await projectService.getProjects()
       dispatch(setProjects(allProjects))
-    } catch (e) {
-      console.log(e)
+    } catch (e: any) {
+      dispatch(setFetchProjectsError(getErrorMsg(e)))
     }
   }
 }
