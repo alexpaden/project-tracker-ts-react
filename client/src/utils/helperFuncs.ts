724363ec03e75b8d08f91f90453d74bcb08dab5a
Alex Paden <padenfool@gmail.com>
2023-01-13 08:51:51 -0600

    Create error message helperFunc

--- client/src/utils/helperFuncs.ts
+++ client/src/utils/helperFuncs.ts
@@ -1,5 +1,22 @@
 import { format, formatDistanceToNowStrict } from 'date-fns'
 
+interface ReturnedError {
+  response?: {
+    data?: {
+      message: string
+    }
+  }
+  message: string
+}
+
+export const getErrorMsg = (err: ReturnedError) => {
+  if (err?.response?.data?.message) {
+    return err.response.data.message
+  } else {
+    return err.message
+  }
+}
+
 export const truncateString = (text: string, maxCharLimit: number) => {
   return text.length < maxCharLimit ? text : text.slice(0, maxCharLimit) + '...'
 }
