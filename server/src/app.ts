e3b0aedd11569f5833100daa5866bd47d53655da
Alex Paden <padenfool@gmail.com>
2023-01-15 09:46:53 -0600

    Unknown endpoint and errorHandler middleware
    registered in app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import cors from 'cors'
 import 'express-async-errors'
 import projectRoutes from './routes/project'
+import unknownEndPointHandler from './middleware/unknownEndpoint'
+import errorHandler from './middleware/errorHandler'
 
 const app = express()
 
@@ -9,5 +11,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/projects', projectRoutes)
+
+app.use(unknownEndPointHandler)
+app.use(errorHandler)
 
 export default app
