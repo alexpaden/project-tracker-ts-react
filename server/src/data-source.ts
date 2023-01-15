ad786efcfa851b869eaa77c81383d41583c98c7d
Alex Paden <padenfool@gmail.com>
2023-01-15 09:48:30 -0600

    Register User, Member entities
    in datasource

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -3,13 +3,15 @@ import { DataSource } from 'typeorm'
 import { Project } from './entity/Project'
 import { Bug } from './entity/Bug'
 import { Note } from './entity/Note'
+import { Member } from './entity/Member'
+import { User } from './entity/User'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: 'database.sqlite',
   synchronize: true,
   logging: false,
-  entities: [Project, Bug, Note],
+  entities: [Project, Bug, Note, User, Member],
   migrations: [],
   subscribers: [],
 })
