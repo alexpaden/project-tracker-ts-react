4cf3b1ab65c28ed0de2d7ac5fcbae3fecd90c615
Alex Paden <padenfool@gmail.com>
2023-01-16 11:54:52 -0600

    Update note entity

--- server/src/entity/Note.ts
+++ server/src/entity/Note.ts
@@ -1,12 +1,15 @@
-import { Bug } from './Bug'
 import {
   BaseEntity,
   Entity,
   PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
   Column,
   ManyToOne,
   JoinColumn,
 } from 'typeorm'
+import { Bug } from './Bug'
+import { User } from './User'
 
 @Entity({ name: 'notes' })
 export class Note extends BaseEntity {
@@ -16,9 +19,21 @@ export class Note extends BaseEntity {
   @Column()
   body: string
 
+  @ManyToOne(() => User, (user) => user)
+  @JoinColumn({ name: 'authorId' })
+  author: User
+  @Column()
+  authorId: string
+
   @ManyToOne(() => Bug, (bug) => bug)
   @JoinColumn({ name: 'bugId' })
   bug: Bug
   @Column()
   bugId: string
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  @UpdateDateColumn()
+  updatedAt: Date
 }
