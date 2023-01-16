ef4adf2b524817c683be0e183c84db45fab889b4
Alex Paden <padenfool@gmail.com>
2023-01-16 13:53:55 -0600

    Clone Projects Table in ProjectsPage
    simple table to work with

--- client/src/pages/ProjectsPage.tsx
+++ client/src/pages/ProjectsPage.tsx
@@ -1,8 +1,66 @@
+import { Link as RouterLink } from 'react-router-dom'
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Link,
+  Paper,
+} from '@material-ui/core'
+import '../styles/projects.css'
+import { formatDateTime, truncateString } from '../utils/helperFuncs'
+import { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  selectProjectsState,
+  fetchProjects,
+} from '../redux/slices/projectsSlice'
+
+const tableHeaders = ['Name', 'Bugs', 'Added']
+
 const ProjectsPage = () => {
+  const { projects } = useSelector(selectProjectsState)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProjects())
+  }, [dispatch])
+
   return (
-    <div>
-      <h1>Projects</h1>
-    </div>
+    <Paper className="MiniTable">
+      <Table>
+        <TableHead>
+          <TableRow>
+            {tableHeaders.map((t) => (
+              <TableCell key={t} align="center">
+                {t}
+              </TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {projects.map((p) => (
+            <TableRow key={p.id}>
+              <TableCell align="center">
+                <Link
+                  component={RouterLink}
+                  to={`/projects/${p.id}`}
+                  color="secondary"
+                >
+                  {truncateString(p.name, 30)}
+                </Link>
+              </TableCell>
+              <TableCell align="center">bugs#</TableCell>
+              <TableCell align="center">
+                {formatDateTime(p.createdAt)}
+              </TableCell>
+              <TableCell align="center"></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Paper>
   )
 }
 
