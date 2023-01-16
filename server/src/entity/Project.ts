a76c2ae60a955057787e9c6bb1c4e8ce8c543962
Alex Paden <padenfool@gmail.com>
2023-01-16 11:56:01 -0600

    Update project entity

--- server/src/entity/Project.ts
+++ server/src/entity/Project.ts
@@ -1,8 +1,20 @@
-import { Entity, Column } from 'typeorm'
+import { Entity, Column, ManyToOne, JoinColumn, OneToMany } from 'typeorm'
 import Model from './Model'
+import { User } from './User'
+import { Bug } from './Bug'
 
 @Entity({ name: 'projects' })
 export class Project extends Model {
   @Column({ type: 'varchar', length: 60 })
   name: string
+
+  @ManyToOne(() => User, (user) => user)
+  @JoinColumn({ name: 'createdById' })
+  createdBy: User
+  @Column()
+  createdById: string
+
+  @OneToMany(() => Bug, (bugs) => bugs.project)
+  @JoinColumn()
+  bugs: Bug[]
 }
