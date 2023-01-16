d232cf6156ddd5a0810932803c3d847adcb24d42
Alex Paden <padenfool@gmail.com>
2023-01-16 10:35:01 -0600

    Update data-source from sqlite to postgresql

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -4,10 +4,16 @@ import { Project } from './entity/Project'
 import { Bug } from './entity/Bug'
 import { Note } from './entity/Note'
 import { User } from './entity/User'
+import dotenv from 'dotenv'
+dotenv.config()
 
 export const AppDataSource = new DataSource({
-  type: 'sqlite',
-  database: 'database.sqlite',
+  type: 'postgres',
+  host: process.env.LDB_HOST as string,
+  port: process.env.LDB_PORT as unknown as number,
+  username: process.env.LDB_USERNAME as string,
+  password: process.env.LDB_PASSWORD as string,
+  database: process.env.LDB_DATABASE as string,
   synchronize: true,
   logging: false,
   entities: [Project, Bug, Note, User],
