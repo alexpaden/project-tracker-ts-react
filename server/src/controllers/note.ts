fed51f502514f83ee1336612aec0fe9202c03c27
Alex Paden <padenfool@gmail.com>
2023-01-16 12:12:57 -0600

    Update note controller

--- server/src/controllers/note.ts
+++ server/src/controllers/note.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from 'express'
 import { Note } from '../entity/Note'
+import { Project } from '../entity/Project'
 
-const fieldsToSelect = ['note.id', 'note.bugId', 'note.body']
+const fieldsToSelect = [
+  'note.id',
+  'note.bugId',
+  'note.body',
+  'note.createdAt',
+  'note.updatedAt',
+  'author.id',
+  'author.username',
+]
 
 export const getNotes = async (req: Request, res: Response) => {
   const { bugId } = req.params
@@ -23,19 +32,35 @@ export const postNote = async (req: Request, res: Response) => {
       .send({ message: 'Note body field must not be empty.' })
   }
 
-  const newNote = Note.create({ body, bugId })
+  const newNote = Note.create({ body, authorId: req.user, bugId })
   await newNote.save()
 
   res.status(201).json(newNote)
 }
 
 export const deleteNote = async (req: Request, res: Response) => {
-  const { noteId } = req.params
+  const { projectId, noteId } = req.params
+
+  const targetProject = await Project.findOne({
+    where: { id: projectId },
+    relations: ['members'],
+  })
+
+  if (!targetProject) {
+    return res.status(404).send({ message: 'Invalid project ID.' })
+  }
 
   const targetNote = await Note.findOneBy({ id: Number(noteId) })
 
   if (!targetNote) {
     return res.status(404).send({ message: 'Invalid note ID.' })
+  }
+
+  if (
+    targetNote.authorId !== req.user &&
+    targetProject.createdById !== req.user
+  ) {
+    return res.status(401).send({ message: 'Access is denied.' })
   }
 
   await targetNote.remove()
@@ -56,6 +81,10 @@ export const updateNote = async (req: Request, res: Response) => {
 
   if (!targetNote) {
     return res.status(404).send({ message: 'Invalid note ID.' })
+  }
+
+  if (targetNote.authorId !== req.user) {
+    return res.status(401).send({ message: 'Access is denied.' })
   }
 
   targetNote.body = body
