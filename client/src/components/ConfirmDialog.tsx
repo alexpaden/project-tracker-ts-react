b606242fd5aa98fc250e1984a28d25ed45e99c37
Alex Paden <padenfool@gmail.com>
2023-01-22 14:26:06 -0600

    Create ProjectDetailPage

---
+++ client/src/components/ConfirmDialog.tsx
@@ -0,0 +1,141 @@
+import { useState } from 'react'
+import HideOnScroll from './HideOnScroll'
+import { TriggerButtonTypes } from './types'
+import {
+  Button,
+  IconButton,
+  MenuItem,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Typography,
+  Fab,
+} from '@material-ui/core'
+import { useDialogStyles } from '../styles/muiStyles'
+
+const ConfirmDialog: React.FC<{
+  title: string
+  contentText: string
+  actionBtnText: string
+  triggerBtn: TriggerButtonTypes
+  actionFunc: () => void
+}> = ({ title, contentText, actionBtnText, triggerBtn, actionFunc }) => {
+  const classes = useDialogStyles()
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  const handleDialogClose = () => {
+    setDialogOpen(false)
+  }
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true)
+    triggerBtn.type === 'menu' && triggerBtn.closeMenu()
+  }
+
+  const handleConfirmedAction = () => {
+    actionFunc()
+    handleDialogClose()
+  }
+
+  const triggerButton = () => {
+    if (triggerBtn.type === 'icon') {
+      return (
+        <IconButton
+          color={triggerBtn.color || 'primary'}
+          onClick={handleDialogOpen}
+          size={triggerBtn.size || 'medium'}
+          className={triggerBtn.className}
+          style={triggerBtn.style}
+        >
+          <triggerBtn.icon fontSize={triggerBtn.iconSize || 'default'} />
+        </IconButton>
+      )
+    } else if (triggerBtn.type === 'menu') {
+      return (
+        <MenuItem onClick={handleDialogOpen}>
+          <triggerBtn.icon style={triggerBtn.iconStyle} />
+          {triggerBtn.text}
+        </MenuItem>
+      )
+    } else if (triggerBtn.type === 'fab') {
+      return (
+        <HideOnScroll>
+          <Fab
+            variant={triggerBtn.variant || 'round'}
+            size={triggerBtn.size || 'medium'}
+            color={triggerBtn.color || 'primary'}
+            className={classes.fab}
+            onClick={handleDialogOpen}
+          >
+            <triggerBtn.icon />
+            {triggerBtn.variant === 'extended' && triggerBtn.text}
+          </Fab>
+        </HideOnScroll>
+      )
+    } else if (triggerBtn.type === 'round') {
+      return (
+        <Button
+          color={triggerBtn.color || 'primary'}
+          variant={triggerBtn.variant || 'contained'}
+          size={triggerBtn.size || 'medium'}
+          onClick={handleDialogOpen}
+          style={triggerBtn.style}
+          className={classes.roundIconButton}
+        >
+          <triggerBtn.icon />
+        </Button>
+      )
+    } else {
+      return (
+        <Button
+          color={triggerBtn.color || 'primary'}
+          variant={triggerBtn.variant || 'contained'}
+          size={triggerBtn.size || 'medium'}
+          startIcon={<triggerBtn.icon />}
+          onClick={handleDialogOpen}
+          style={triggerBtn.style}
+          className={triggerBtn.className}
+        >
+          {triggerBtn.text}
+        </Button>
+      )
+    }
+  }
+
+  return (
+    <div style={{ display: 'inline' }}>
+      {triggerButton()}
+      <Dialog open={dialogOpen} onClose={handleDialogOpen}>
+        <DialogTitle disableTypography>
+          <Typography color="secondary" variant="h6">
+            {title}
+          </Typography>
+        </DialogTitle>
+        <DialogContent dividers>
+          <Typography>{contentText}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            onClick={handleDialogClose}
+            color="secondary"
+            variant="outlined"
+            size="small"
+          >
+            Cancel
+          </Button>
+          <Button
+            onClick={handleConfirmedAction}
+            color="primary"
+            variant="contained"
+            size="small"
+          >
+            {actionBtnText}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default ConfirmDialog
---
+++ client/src/pages/ProjectDetailPage.tsx
@@ -0,0 +1,125 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectProjectById, deleteProject } from '../redux/slices/projectsSlice'
+import { selectAuthState } from '../redux/slices/authSlice'
+import { RootState } from '../redux/store'
+import ProjectForm from '../components/ProjectForm'
+import ConfirmDialog from '../components/ConfirmDialog'
+import FormDialog from '../components/FormDialog'
+import { formatDateTime } from '../utils/helperFuncs'
+
+import {
+  Paper,
+  Typography,
+  Button,
+  Divider,
+  useMediaQuery,
+} from '@material-ui/core'
+import { useTheme } from '@material-ui/core/styles'
+import { useMainPageStyles } from '../styles/muiStyles'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteOutlineIcon from '@material-ui/icons/DeleteOutline'
+import ErrorBox from '../components/ErrorBox'
+
+const ProjectDetailsPage = () => {
+  const classes = useMainPageStyles()
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+  const { projectId } = useParams()
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { user } = useSelector(selectAuthState)
+  const projectInState = useSelector((state: RootState) =>
+    selectProjectById(state, projectId ? projectId : '')
+  )
+
+  if (!projectInState) {
+    return (
+      <div className={classes.root}>
+        <Paper className={classes.notFoundPaper}>
+          <Typography
+            variant="h6"
+            color="secondary"
+            className={classes.error404Text}
+            style={{ marginTop: '5em' }}
+          >
+            404: Project Not Found!
+          </Typography>
+        </Paper>
+      </div>
+    )
+  }
+
+  const { id, name, createdAt, updatedAt, createdBy } = projectInState
+
+  const isAdmin = createdBy.id === user?.id
+
+  const handleDeleteProject = async () => {
+    try {
+      await dispatch(deleteProject(id))
+      navigate('/projects')
+    } catch (err) {
+      ;<ErrorBox errorMsg={err} clearErrorMsg={() => {}} />
+    }
+  }
+
+  const adminBtns = () => {
+    //if (!isAdmin) return null
+
+    return (
+      <>
+        <ConfirmDialog
+          title="Confirm Delete Project"
+          contentText="Are you sure you want to permanently delete your project?"
+          actionBtnText="Delete Project"
+          triggerBtn={{
+            type: isMobile ? 'round' : 'normal',
+            text: 'Delete Project',
+            icon: DeleteOutlineIcon,
+          }}
+          actionFunc={handleDeleteProject}
+        />
+      </>
+    )
+  }
+
+  return (
+    <div className={classes.root}>
+      <Paper className={classes.detailsHeader}>
+        <div className={classes.flexHeader}>
+          <Typography
+            variant={isMobile ? 'h5' : 'h4'}
+            color="secondary"
+            style={{ marginRight: '0.2em' }}
+          >
+            <strong>{name}</strong>
+          </Typography>
+          {isAdmin && (
+            <FormDialog
+              triggerBtn={{ type: 'icon', icon: EditIcon, size: 'small' }}
+              title="Edit the project name"
+            >
+              <ProjectForm editMode="name" currentName={name} projectId={id} />
+            </FormDialog>
+          )}
+        </div>
+        <Divider style={{ margin: '0.5em 0' }} />
+        <Typography variant="subtitle2" color="secondary">
+          Admin: <strong>{createdBy.username}</strong>
+        </Typography>
+        <Typography variant="subtitle2" color="secondary">
+          Created At: <em>{formatDateTime(createdAt)}</em>
+        </Typography>
+        {createdAt !== updatedAt && (
+          <Typography variant="subtitle2" color="secondary">
+            Updated At: <em>{formatDateTime(updatedAt)}</em>
+          </Typography>
+        )}
+        <div className={classes.btnsWrapper}>{adminBtns()}</div>
+      </Paper>
+      {/*<BugsCard projectId={projectId} isMobile={isMobile} />*/}
+    </div>
+  )
+}
+
+export default ProjectDetailsPage
