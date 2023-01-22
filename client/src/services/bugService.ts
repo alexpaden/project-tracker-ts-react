eacb806cc52689eaaac17a1b55df6bcda1838004
Alex Paden <padenfool@gmail.com>
2023-01-22 14:32:41 -0600

    Create bugService.ts

---
+++ client/src/services/bugService.ts
@@ -0,0 +1,57 @@
+import axios from 'axios'
+import http from '../http-common'
+import { BugPayload } from '../redux/types'
+
+const getBugs = async (projectId: string) => {
+  const response = await http.get(`/projects/${projectId}/bugs`)
+  return response.data
+}
+
+const createBug = async (projectId: string, bugData: BugPayload) => {
+  const response = await http.post(`/projects/${projectId}/bugs`, bugData)
+  return response.data
+}
+
+const updateBug = async (
+  projectId: string,
+  bugId: string,
+  bugData: BugPayload
+) => {
+  const response = await http.put(
+    `/projects/${projectId}/bugs/${bugId}`,
+    bugData
+  )
+  return response.data
+}
+
+const deleteBug = async (projectId: string, bugId: string) => {
+  const response = await http.delete(`/projects/${projectId}/bugs/${bugId}`)
+  return response.data
+}
+
+const closeBug = async (projectId: string, bugId: string) => {
+  const response = await axios.post(
+    `/projects/${projectId}/bugs/${bugId}/close`,
+    null
+  )
+  return response.data
+}
+
+const reopenBug = async (projectId: string, bugId: string) => {
+  const response = await axios.post(
+    `/projects/${projectId}/bugs/${bugId}/reopen`,
+    null
+  )
+  return response.data
+}
+
+const bugService = {
+  getBugs,
+  createBug,
+  updateBug,
+  deleteBug,
+  closeBug,
+  reopenBug,
+}
+
+export default bugService
