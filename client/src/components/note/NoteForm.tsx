d9b851e3fb3710d478e1fdb9becaa4e32735c7d2
Alex Paden <padenfool@gmail.com>
2023-01-24 12:48:10 -0600

    Create Notes cards and form

---
+++ client/src/components/note/NoteForm.tsx
@@ -0,0 +1,108 @@
+import { useForm } from 'react-hook-form'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  createNote,
+  editNote,
+  clearSubmitBugError,
+  selectBugsState,
+} from '../../redux/slices/bugsSlice'
+import ErrorBox from '../../components/ErrorBox'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import { TextField, Button, InputAdornment } from '@material-ui/core'
+import { useFormStyles } from '../../styles/muiStyles'
+import CommentIcon from '@material-ui/icons/Comment'
+
+const validationSchema = yup.object({
+  body: yup.string().required('Required'),
+})
+
+interface NoteFormProps {
+  closeDialog?: () => void
+  projectId: string
+  bugId: string
+  isEditMode: boolean
+  currentBody?: string
+  noteId?: number
+}
+
+const NoteForm = ({
+  closeDialog,
+  isEditMode,
+  projectId,
+  bugId,
+  currentBody,
+  noteId,
+}: NoteFormProps) => {
+  const classes = useFormStyles()
+  const dispatch = useDispatch()
+  const { submitError, submitLoading } = useSelector(selectBugsState)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      body: currentBody || '',
+    },
+  })
+
+  const handleCreateNote = ({ body }: { body: string }) => {
+    dispatch(createNote(projectId, bugId, body, closeDialog))
+  }
+
+  const handleUpdateNote = ({ body }: { body: string }) => {
+    dispatch(editNote(projectId, bugId, noteId as number, body, closeDialog))
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(isEditMode ? handleUpdateNote : handleCreateNote)}
+    >
+      <TextField
+        multiline
+        minRows={1}
+        maxRows={4}
+        {...register('body')}
+        name="body"
+        placeholder="Type a note..."
+        required
+        fullWidth
+        type="text"
+        label="Note"
+        variant="outlined"
+        error={'body' in errors}
+        helperText={'body' in errors ? errors.body?.message : ''}
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <CommentIcon color="primary" />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <Button
+        size="large"
+        color="primary"
+        variant="contained"
+        fullWidth
+        className={classes.submitBtn}
+        type="submit"
+        disabled={submitLoading}
+      >
+        {isEditMode ? 'Update Note' : 'Submit Note'}
+      </Button>
+      {submitError && (
+        <ErrorBox
+          errorMsg={submitError}
+          clearErrorMsg={() => dispatch(clearSubmitBugError())}
+        />
+      )}
+    </form>
+  )
+}
+
+export default NoteForm
---
+++ client/src/components/note/NotesCard.tsx
@@ -0,0 +1,180 @@
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectAuthState } from '../../redux/slices/authSlice'
+import { selectProjectById } from '../../redux/slices/projectsSlice'
+import { deleteNote } from '../../redux/slices/bugsSlice'
+import { Note, NoteSortValues } from '../../redux/types'
+import { RootState } from '../../redux/store'
+import SortBar from '../../components/SortBar'
+import sortNotes from '../../utils/sortNotes'
+import NoteForm from './NoteForm'
+import ConfirmDialog from '../../components/ConfirmDialog'
+import FormDialog from '../../components/FormDialog'
+import InfoText from '../../components/InfoText'
+import { formatTimeAgo } from '../../utils/helperFuncs'
+
+import { Paper, Typography, Avatar, Divider } from '@material-ui/core'
+import { useMainPageStyles } from '../../styles/muiStyles'
+import ForumOutlinedIcon from '@material-ui/icons/ForumOutlined'
+import EditIcon from '@material-ui/icons/Edit'
+import DeleteIcon from '@material-ui/icons/Delete'
+import CommentOutlinedIcon from '@material-ui/icons/CommentOutlined'
+
+const menuItems = [
+  { value: 'newest', label: 'Newest' },
+  { value: 'oldest', label: 'Oldest' },
+  { value: 'updated', label: 'Recently Updated' },
+]
+
+interface NotesCardProps {
+  notes: Note[]
+  projectId: string
+  bugId: string
+  isMobile: boolean
+}
+
+const NotesCard = ({ notes, projectId, bugId, isMobile }: NotesCardProps) => {
+  const classes = useMainPageStyles()
+  const dispatch = useDispatch()
+  const { user } = useSelector(selectAuthState)
+  const project = useSelector((state: RootState) =>
+    selectProjectById(state, projectId)
+  )
+  const [sortBy, setSortBy] = useState<NoteSortValues>('newest')
+
+  const handleSortChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    setSortBy(e.target.value as NoteSortValues)
+  }
+
+  const sortedNotes = sortNotes(notes, sortBy)
+
+  const handleDeleteNote = (noteId: number) => {
+    dispatch(deleteNote(projectId, bugId, noteId))
+  }
+
+  return (
+    <Paper className={classes.notesPaper}>
+      <div className={classes.flexInput}>
+        <Typography
+          variant={isMobile ? 'h6' : 'h5'}
+          color="secondary"
+          className={classes.flexHeader}
+        >
+          <ForumOutlinedIcon
+            fontSize={isMobile ? 'default' : 'large'}
+            style={{ marginRight: '0.2em' }}
+          />
+          Notes
+        </Typography>
+        <div className={classes.sortNotesInput}>
+          <SortBar
+            sortBy={sortBy}
+            handleSortChange={handleSortChange}
+            menuItems={menuItems}
+            label="Notes"
+            size="small"
+          />
+        </div>
+      </div>
+      <FormDialog
+        triggerBtn={
+          isMobile
+            ? { type: 'fab', variant: 'round', icon: CommentOutlinedIcon }
+            : {
+                type: 'normal',
+                text: 'Leave A Note',
+                icon: CommentOutlinedIcon,
+                size: 'large',
+                style: { marginTop: '1em' },
+              }
+        }
+        title="Post a note"
+      >
+        <NoteForm isEditMode={false} projectId={projectId} bugId={bugId} />
+      </FormDialog>
+      <div className={classes.notesWrapper}>
+        <Divider />
+        {sortedNotes.length === 0 && (
+          <InfoText
+            text="No notes added yet."
+            variant={isMobile ? 'h6' : 'h5'}
+          />
+        )}
+        {sortedNotes.map((n) => (
+          <div key={n.id}>
+            <div className={classes.singleNote}>
+              <Avatar className={classes.avatar}>
+                {n.author.username.slice(0, 1)}
+              </Avatar>
+              <div>
+                <Typography color="secondary" variant="caption">
+                  {n.author.username}
+                </Typography>
+                <Typography color="secondary" variant="caption">
+                  <em> • {formatTimeAgo(n.createdAt)} ago</em>
+                </Typography>
+                {n.updatedAt !== n.createdAt && (
+                  <Typography color="secondary" variant="caption">
+                    {' '}
+                    • updated <em>{formatTimeAgo(n.updatedAt)} ago</em>
+                  </Typography>
+                )}
+                <Typography
+                  color="secondary"
+                  variant="subtitle1"
+                  className={classes.noteBody}
+                >
+                  {n.body}
+                </Typography>
+                <div className={classes.notesBtnWrapper}>
+                  {n.author.id === user?.id && (
+                    <FormDialog
+                      triggerBtn={{
+                        type: 'normal',
+                        text: 'Edit',
+                        icon: EditIcon,
+                        variant: 'outlined',
+                        size: 'small',
+                        style: { marginRight: '1em' },
+                        color: 'secondary',
+                      }}
+                      title="Edit the note"
+                    >
+                      <NoteForm
+                        isEditMode={true}
+                        projectId={projectId}
+                        bugId={bugId}
+                        noteId={n.id}
+                        currentBody={n.body}
+                      />
+                    </FormDialog>
+                  )}
+                  {(n.author.id === user?.id ||
+                    user?.id === project?.createdBy.id) && (
+                    <ConfirmDialog
+                      title="Confirm Delete Note"
+                      contentText="Are you sure you want to delete the note?"
+                      actionBtnText="Delete Note"
+                      triggerBtn={{
+                        type: 'normal',
+                        text: 'Delete',
+                        icon: DeleteIcon,
+                        variant: 'outlined',
+                        size: 'small',
+                        color: 'secondary',
+                      }}
+                      actionFunc={() => handleDeleteNote(n.id)}
+                    />
+                  )}
+                </div>
+              </div>
+            </div>
+            <Divider />
+          </div>
+        ))}
+      </div>
+    </Paper>
+  )
+}
+
+export default NotesCard
